885c90d5430517e8533c105f69ddd46d153f9bdb
Imani Ward <idward712@gmail.com>
2022-12-22 14:16:12 -0500

    o

--- RPS/Sign.swift
+++ RPS/Sign.swift
@@ -7,6 +7,20 @@
 
 import Foundation
 
+
+func randomSign() -> Sign {
+    let sign = Int.random(in: 0...2)
+    
+        if sign == 0 {
+            return Sign.rock
+        } else if sign == 1 {
+            return .paper
+        } else {
+            return .scissors
+        }
+    }
+
+
 enum Sign {
     case rock
     case paper
@@ -23,8 +37,42 @@ enum Sign {
         }
     }
     
-    func checkWin(sign: Sign){
-        if self == 
+    func checkWin(sign: Sign)-> GameState{
+        if self == .rock && sign == .rock{
+            return .draw
+        }
+        if self == .rock && sign == .paper{
+            return .lose
+        }
+        if self == .rock && sign == .scissors{
+            return .win
+        }
+        
+        
+        if self == .scissors && sign == .scissors{
+            return .draw
+        }
+        if self == .scissors && sign == .paper{
+            return .win
+        }
+        if self == .scissors && sign == .rock{
+            return .lose
+        }
+        
+        
+        if self == .paper && sign == .paper{
+            return .draw
+        }
+        if self == .paper && sign == .scissors{
+            return .lose
+        }
+        if self == .paper && sign == .rock{
+            return .win
+        }
+        
+        return .draw
         
     }
+    
 }
+
--- RPS/ViewController.swift
+++ RPS/ViewController.swift
@@ -14,6 +14,42 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
     }
 
-
+    @IBOutlet var computerSign: UILabel!
+    @IBOutlet var gameStatus: UILabel!
+    @IBOutlet var rock: UIButton!
+    @IBOutlet var paper: UIButton!
+    @IBOutlet var scissors: UIButton!
+    
+    
+    
+    
+    
+    
+    
+    
+    @IBAction func rock(_ sender: Any) {
+    }
+    
+    @IBAction func paper(_ sender: Any) {
+    }
+    
+    @IBAction func scissors(_ sender: Any) {
+    }
+    
+    
+    @IBAction func playAgain(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 }
 
